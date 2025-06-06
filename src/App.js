c0be5cf96441ae9c0e495b35f7942f7bfe41e374
BryanCaballero0922 <bryancaballero0622@gmail.com>
2025-06-05 20:30:47 -0600

    no message

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <BrowserRouter>
       <Routes>
        
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
 
         
         <Route path="/*" element={
